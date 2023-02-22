d9c68c4f27769c466dfedefa22de0185d8704238
hinoja <hinoja2@gmail.com>
2023-02-22 11:45:04 +0100

    authentification  with disable case

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $remember = (bool) $request->remember;
+
+        if (! Auth::attempt(['email' => $request->email, 'password' => $request->password, 'active' => 1], $remember)) {
+            $user = User::where('email', $request->email)->first();
+            if (Hash::check($request->password, $user->password)) {
+                $message = 'auth.status';
+            } else {
+                $message = 'auth.failed';
+            }
+
+            throw ValidationException::withMessages([
+                'email' => trans($message),
+            ]);
+        }
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,6 +13,8 @@ return [
     |
     */
 
+    'status' => 'Your account is disable, Please contact administrator.',
+    'failed' => 'These credentials do not match our records.',
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
