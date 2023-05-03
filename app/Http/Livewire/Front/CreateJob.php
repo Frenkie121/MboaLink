54435b9c85d4e7224d185c8a284f2c82a2a5e7bd
Frenkie121 <danipacific232@gmail.com>
2023-05-03 15:31:17 +0100

    Uncomment mail create job notification

--- app/Http/Livewire/Front/CreateJob.php
+++ app/Http/Livewire/Front/CreateJob.php
@@ -170,7 +170,7 @@ class CreateJob extends Component
         }
 
         // II. MAILS
-        // Notification::send([User::query()->firstWhere('role_id', 1), $user], new PostJobNotification($job));
+        Notification::send([User::query()->firstWhere('role_id', 1), $user], new PostJobNotification($job));
 
         alert('', trans('Your job has been successfully registered. It will be studied and you will be informed of its publication or not as soon as possible. An email related to this action has been sent to you, please check your mailbox.'), 'success')->autoclose(20000);
 
