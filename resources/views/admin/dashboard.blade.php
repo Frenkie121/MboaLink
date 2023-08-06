18f2eb2fa12d0a87565d49b0703e27430c3f2a5a
janohi <hinoja2@gmail.com>
2023-08-06 18:35:42 +0200

    charts in progress

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,145 +4,47 @@
 
 @section('content')
 
-    <div class="section-header">
-        <h1>@lang('Dashboard')</h1>
-    </div>
-    {{--  ---------- Users -------------- --}}
-    <h4 class="text-center"> @lang('Statitics Users')</h4> <br>
-    <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
-                    <i class="far fa-user"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total @lang('Users')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $users }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-danger">
-                    <i class="far fa-newspaper"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>@lang('Subscribers')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $subscribers }}
-                    </div>
-                </div>
-            </div>
-        </div>
+    {{-- <div class="section-header"> --}}
+    {{-- <h1>@lang('Dashboard')</h1> --}}
+    <x-admin.section-header :title="__('Statistics')" :previousTitle="__('Dashboard')" :previousRouteName="route('admin.dashboard')" />
+    {{-- </div> --}}
 
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                    <i class="far fa-file"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>@lang('User Not Suscribe')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $users - $subscribers }}
-                    </div>
-                </div>
+
+    <div class="section-body">
+        <div class="row">
+            <div class="col-md-6">
+                {!! $ChartUserData->container() !!}
             </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                    <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Online Users</h4>
-                    </div>
-                    <div class="card-body">
-                        47
-                    </div>
-                </div>
+
+            <div class="col-md-6">
+                {!! $usersChart->container() !!}
             </div>
+            <div class="col-md-6">
+                {!! $chart2->container() !!}
+            </div>
+
         </div>
     </div>
-    {{-- --------------- Jobs -------------------- --}}
-    <h4 class="text-center"> @lang('Statitics Jobs')</h4><br>
-
-    <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
-                    <i class="fab fa fa-briefcase"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total @lang('Jobs')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $jobs }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-danger">
-                    <i class="fab fa-phoenix-framework"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>@lang('Disabled Jobs')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $jobs - $enableJobs }}
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                    <i class="far fa-file"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>@lang('Enable Jobs')</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ $enableJobs }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                    <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Online Users</h4>
-                    </div>
-                    <div class="card-body">
-                        47
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-    </div>
-
 
 @endsection
 
 @push('js')
-    <!-- Page Specific JS File -->
-    <script src="{{ asset('assets/back/js/page/index-0.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js" charset="utf-8"></script>
+    {{-- ChartScript --}}
+    @if ($usersChart)
+        {!! $ChartUserData->script() !!}
+        {!! $usersChart->script() !!}
+        {!! $chart2->script() !!}
+    @endif
+
+
+    <script src="https://unpkg.com/vue"></script>
+    <script>
+        var app = new Vue({
+            el: '#app',
+        });
+    </script>
+    <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
+    {{-- {!! $chart->script() !!} --}}
 @endpush
